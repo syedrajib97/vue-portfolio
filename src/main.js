7bb07dfc04c6ab54d1e7d97fccbc0e2ac8c6ee0b
syed-rajib <soyad.rajib@gigalogy.com>
2024-10-23 22:16:53 +0600

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,47 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import VueLazyload from 'vue-lazyload';
+import VueExpandableImage from "vue-expandable-image";
+Vue.config.productionTip = false;
+var VueScrollTo = require("vue-scrollto");
+
+import { ValidationProvider, extend } from "vee-validate";
+import { required } from "vee-validate/dist/rules";
+
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
+new Vue({
+  router,
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'dist/error.png',
+  loading: 'dist/loading.gif',
+  attempt: 1
+});
+// You can also pass in the default options
+Vue.use(VueScrollTo, {
+  container: "body",
+  duration: 500,
+  easing: "ease",
+  offset: 0,
+  force: true,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true,
+});
+
+// input validation
+extend("required", {
+  ...required,
+  message: "*Please add your message!",
+});
